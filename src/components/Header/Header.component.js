48f4b78a33bc79d91bac4e28e2e14272065f22ca
Arpit Goyal <arpit.go4@gmail.com>
2017-04-10 11:09:22 +0530

    backend http api '/hello' removed

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -4,27 +4,12 @@ import superagent from 'superagent';
 
 export default class Header extends React.Component {
 
-	constructor(props){
-		super(props);
-		this.state = { heading: '' };
-	}
-
-	componentWillMount(){
-        superagent
-            .get('/api/hello')
-            .end((err, res) => {
-                this.setState({
-                    heading: res.body.msg
-                });
-            });
-    }
-
 	render(){
 		return (
 			<div className="container">
 				<div className="row">
 					<h1 className="col-sm-6 col-sm-offset-3 well well-lg text-center">
-						{this.state.heading}
+						React-Redux-Scaffold
 					</h1>
 				</div>
 				<h1 className="page-header">
