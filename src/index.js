986236adaa88e9fb079be61d894436e0358cb02d
Arpit Goyal <arpit.go4@gmail.com>
2017-04-12 11:20:31 +0530

    font-awesome icons added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './index.html';
 import 'jquery';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 import './style.css';
 
