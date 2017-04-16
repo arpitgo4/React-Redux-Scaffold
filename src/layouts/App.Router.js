554b9a58b68e2c1cd24802e1c615ba648c14c4dd
Arpit Goyal <arpit.go4@gmail.com>
2017-04-16 22:56:43 +0530

    unused imports removed from Router. IndexRoute imported, not for aysnc loading at runtime

--- src/layouts/App.Router.js
+++ src/layouts/App.Router.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import AppLayout from './App.layout';
+import Nodejs from '../components/Body/Nodejs.component';
 
 const AppRouter = () => (
 	<Router history={browserHistory}>
 		<Route path="/" component={AppLayout} >
-			<IndexRoute getComponent={() => System.import('../components/Body/Nodejs.component')
-															.then(c => c.default) } />
+			<IndexRoute component={Nodejs} />														
 			<Route path="express" getComponent={() => System.import('../components/Body/Expressjs.component')
 															.then(c => c.default) } />
 			<Route path="mongo" getComponent={() => System.import('../components/Body/Mongodb.component')
-															.then(c => c.default)} />
+															.then(c => c.default) } />
 			<Route path="react" getComponent={() => System.import('../components/Body/Reactjs.component')
-															.then(c => c.default)} />
+															.then(c => c.default) } />
 		</Route>
 	</Router>
 );
