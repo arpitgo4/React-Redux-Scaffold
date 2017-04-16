38f89ee59095f55fb659da69aec670e4c15c9add
Arpit Goyal <arpit.go4@gmail.com>
2017-04-16 18:19:43 +0530

    code spliting added in router. hashHistory removed from imports

--- src/layouts/App.Router.js
+++ src/layouts/App.Router.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Router, Route, IndexRoute, Link, IndexLink, browserHistory, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router';
 
 import AppLayout from './App.layout';
 
 import NodeJS from '../components/Body/Nodejs.component';
-import ExpressJS from '../components/Body/Expressjs.component';
+/*import ExpressJS from '../components/Body/Expressjs.component';
 import ReactJS from '../components/Body/Reactjs.component';
-import MongoDB from '../components/Body/Mongodb.component';
+import MongoDB from '../components/Body/Mongodb.component';*/
 
 const AppRouter = () => (
 	<Router history={browserHistory}>
 		<Route path="/" component={AppLayout} >
 			<IndexRoute component={NodeJS} />
-			<Route path="express" component={ExpressJS} />
-			<Route path="mongo" component={MongoDB} />
-			<Route path="react" component={ReactJS} />
+			<Route path="express" getComponent={() => System.import('../components/Body/Expressjs.component')
+															.then(c => c.default) } />
+			<Route path="mongo" getComponent={() => System.import('../components/Body/Mongodb.component')
+															.then(c => c.default)} />
+			<Route path="react" getComponent={() => System.import('../components/Body/Reactjs.component')
+															.then(c => c.default)} />
 		</Route>
 	</Router>
 );
